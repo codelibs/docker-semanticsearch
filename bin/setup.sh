a16425f9520a3ae77898493e9614a0b4e7009437
Shinsuke Sugaya <shinsuke@apache.org>
2023-09-02 11:54:30 +0900

    add base_dir

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
+base_dir=$(cd $(dirname $0);cd ..;pwd)
 fess_plugins="
 fess-script-groovy:14.10.0
 fess-webapp-semantic-search:14.10.0
 "
 
 echo "Creating directories..."
-mkdir -p ./data/https-portal/ssl_certs
-mkdir -p ./data/fess/opt/fess
-mkdir -p ./data/fess/var/lib/fess
-mkdir -p ./data/fess/var/log/fess
-mkdir -p ./data/fess/usr/share/fess/app/WEB-INF/plugin
-mkdir -p ./data/fess/usr/share/fess/app/WEB-INF/view/semantic
-mkdir -p ./data/opensearch/usr/share/opensearch/data
-mkdir -p ./data/opensearch/usr/share/opensearch/config/dictionary
+mkdir -p ${base_dir}/data/https-portal/ssl_certs
+mkdir -p ${base_dir}/data/fess/opt/fess
+mkdir -p ${base_dir}/data/fess/var/lib/fess
+mkdir -p ${base_dir}/data/fess/var/log/fess
+mkdir -p ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/plugin
+mkdir -p ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/semantic
+mkdir -p ${base_dir}/data/opensearch/usr/share/opensearch/data
+mkdir -p ${base_dir}/data/opensearch/usr/share/opensearch/config/dictionary
 
-rm -f ./data/fess/usr/share/fess/app/WEB-INF/plugin/fess-*.jar
+rm -f ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/plugin/fess-*.jar
 
 for fess_plugin in ${fess_plugins} ; do
   plugin_name=$(echo $fess_plugin | sed -e "s/:.*//")
   plugin_version=$(echo $fess_plugin | sed -e "s/.*://")
+  plugin_file=${base_dir}/data/fess/usr/share/fess/app/WEB-INF/plugin/${plugin_name}-${plugin_version}.jar
   echo "Downloading ${plugin_name} version ${plugin_version}..."
-  curl -o ./data/fess/usr/share/fess/app/WEB-INF/plugin/${plugin_name}-${plugin_version}.jar \
+  curl -o ${plugin_file} \
     https://repo1.maven.org/maven2/org/codelibs/fess/${plugin_name}/${plugin_version}/${plugin_name}-${plugin_version}.jar
 done
 
 if [ $(uname -s) = "Linux" ] ; then
   echo "Changing an owner for directories..."
-  sudo chown -R root ./data/https-portal/ssl_certs
-  sudo chown -R 1001 ./data/fess/opt/fess
-  sudo chown -R 1001 ./data/fess/var/lib/fess
-  sudo chown -R 1001 ./data/fess/var/log/fess
-  sudo chown -R 1001 ./data/fess/usr/share/fess/app/WEB-INF/plugin
-  sudo chown -R 1001 ./data/fess/usr/share/fess/app/WEB-INF/view/semantic
-  sudo chown -R 1000 ./data/opensearch/usr/share/opensearch/data
-  sudo chown -R 1000 ./data/opensearch/usr/share/opensearch/config/dictionary
+  sudo chown -R root ${base_dir}/data/https-portal/ssl_certs
+  sudo chown -R 1001 ${base_dir}/data/fess/opt/fess
+  sudo chown -R 1001 ${base_dir}/data/fess/var/lib/fess
+  sudo chown -R 1001 ${base_dir}/data/fess/var/log/fess
+  sudo chown -R 1001 ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/plugin
+  sudo chown -R 1001 ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/semantic
+  sudo chown -R 1000 ${base_dir}/data/opensearch/usr/share/opensearch/data
+  sudo chown -R 1000 ${base_dir}/data/opensearch/usr/share/opensearch/config/dictionary
 fi
